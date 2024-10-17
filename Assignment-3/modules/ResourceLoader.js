fe80fe5d1baa6ca6dada741793973f9da75c8ef2
Hartley Blakey <github_hartleyblakey@users.noreply.github.com>
2024-10-16 17:09:47 -0700

    remove debug alerts

--- Assignment-3/modules/ResourceLoader.js
+++ Assignment-3/modules/ResourceLoader.js
@@ -34,8 +34,6 @@ class ResourceLoader {
         var res = Object();
         res.frag = this.get(shaderPath(name, "frag"));
         res.vert = this.get(shaderPath(name, "vert"));
-        alert(shaderPath(name, "frag"));
-        alert(res.vert);
         if (!res.frag || !res.vert) {
             alert("unable to find resource at " + path);
         }
